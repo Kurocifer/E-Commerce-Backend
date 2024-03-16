70e2c890c2a3e72370269e1aa7b0b70d8edd772b
Cifer-ti <draculemihawk232@gmail.com>
2024-03-16 22:19:48 +0100

    Add Endpoint security

--- ecommerce-backend/src/main/java/com/tutorial/ecommercebackend/api/controller/product/ProductController.java
+++ ecommerce-backend/src/main/java/com/tutorial/ecommercebackend/api/controller/product/ProductController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/products")
 public class ProductController {
 
   private ProductService productService;
--- ecommerce-backend/src/main/java/com/tutorial/ecommercebackend/api/security/WebSecurityConfig.java
+++ ecommerce-backend/src/main/java/com/tutorial/ecommercebackend/api/security/WebSecurityConfig.java
@@ -4,15 +4,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.security.web.authentication.AuthenticationFilter;
 import org.springframework.stereotype.Component;
 
 @Configuration
 public class WebSecurityConfig {
 
+  private JWTRequestFilter jwtRequestFilter;
+
+  public WebSecurityConfig(JWTRequestFilter jwtRequestFilter) {
+    this.jwtRequestFilter = jwtRequestFilter;
+  }
+
   @Bean
   SecurityFilterChain filterchain(HttpSecurity http) throws Exception {
     http.csrf().disable().cors().disable();
-    http.authorizeHttpRequests().anyRequest().permitAll();
+    http.addFilterBefore(jwtRequestFilter, AuthorizationFilter.class);
+    http.authorizeHttpRequests()
+            .requestMatchers("/products").permitAll()
+            .requestMatchers("/auth/register").permitAll()
+            .anyRequest().authenticated();
 
     return http.build();
   }
